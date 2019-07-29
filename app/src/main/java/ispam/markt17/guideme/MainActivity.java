6d6281d13c6ed0c1b4b74876092791e76fe64717
iSpammer <ispamossama@gmail.com>
2019-07-29 09:30:54 +0200

    Initial commit

---
+++ app/src/main/java/ispam/markt17/guideme/MainActivity.java
@@ -0,0 +1,14 @@
+package ispam.markt17.guideme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
